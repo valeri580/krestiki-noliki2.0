a25b8332ad821bae552fd8d90af826de3435e2b2
Валерий <valeriperehodov@gmail.com>
2025-06-17 08:59:59 +0300

    Update main.py
    add new code for pole

--- main.py
+++ main.py
@@ -14,16 +14,13 @@ buttons = []
 def on_click(row, col):
     pass
 
-
-
-
 for i in range(3):
     row = []
     for j in range(3):
         btn = tk.Button(window, text="", font=("Arial", 20), width=5, height=2, command=lambda r=i, c=j: on_click(r, c))
         btn.grid(row=i, column=j)
         row.append(btn)
-        buttons.append(row)
+    buttons.append(row)
 
 window.mainloop()
 
