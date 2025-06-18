8ef85189f366bdd389926b80899db16a9bd4725f
Валерий <valeriperehodov@gmail.com>
2025-06-18 16:34:22 +0300

    Merge remote-tracking branch 'origin/logic' into logic

--- main.py
+++ main.py
@@ -7,12 +7,37 @@ window.title("Крестики-нолики")
 window.geometry("300x350")
 
 current_player = "X"
-
 buttons = []
 
 
+def check_winner():
+    for i in range(3):
+        if buttons[i][0]["text"] == buttons[i][1]["text"] == buttons[i][2]["text"] != "":
+            return True
+        if buttons[0][i]["text"] == buttons[1][i]["text"] == buttons[2][i]["text"] != "":
+            return True
+
+
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        return True
+    if buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        return True
+    return False
+
+
 def on_click(row, col):
-    pass
+    global current_player
+
+    if buttons[row][col]['text'] != "":
+        return
+
+    buttons[row][col]['text'] = current_player
+
+    if check_winner():
+        messagebox.showinfo("Игра окончена", f"Игрок {current_player} победил!")
+
+    current_player = "0" if current_player == "X" else "X"
+
 
 for i in range(3):
     row = []
